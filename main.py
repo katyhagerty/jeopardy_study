d9a57515ae52269d1a8f879bc477a9b1b774e93b
katyhagerty <katyhagerty93@gmail.com>
2023-08-15 22:10:25 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 15 21:37:39 2023
+
+@author: katyhagerty
+"""
+
+import streamlit as st
+
+loc = 'clues.csv'
+
+st.cache()
+def load_data(loc):
+    return pd.read_csv(loc)
+
+data = load_data(loc)
+
+def display_clue(df):
+    category = df.category.iloc[0].upper()
+    clue = df.text.iloc[0]
+    return category,clue
+    
+def display_target(df):
+    return df.target.iloc[0]
+    
+def pick_clue(data):
+    return data.sample(1)
+
+df = pick_clue(data)
+category, clue = display_clue(df)
+target = display_target(df)
+
+st.header(category)
+st.write(clue)
+target = st.write('')
+button = st.button('Show answer')
+new_clue = st.button('New clue')
+if button:
+    target.write(target)
+    
+if new_clue:
+    df = pick_clue(data)
+    category, clue = display_clue(df)
+    target = display_target(df)
