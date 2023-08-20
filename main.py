69cb724b3b306660a42419a1a7bb9d42dbfdf9da
Katherine Hagerty <katherine.hagerty@boeing.com>
2023-08-20 11:29:31 -0500

    Added callback for new_clue button

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ header = st.header(st.session_state.category)
 clue_text = st.write(st.session_state.clue)
 
 button = st.button('Show answer')#, on_click= show_answer())
-new_clue = st.button('New clue')#, on_click = update())
+new_clue = st.button('New clue', on_click = update())
 
 if button:
     target = st.write(st.session_state.answer)
