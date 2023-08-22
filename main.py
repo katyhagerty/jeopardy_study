e9aca541eb66c3d71a9228c3c7bcbd878bda41d5
katyhagerty <katyhagerty93@gmail.com>
2023-08-21 22:24:24 -0500

    Fixed session state

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import streamlit as st
 import pandas as pd
 # from streamlit_toggle import st_toggle_switch
 
-loc = 'clues.csv'
+loc = 'all_clues.csv'
 
 st.cache_data()
 def load_data(loc):
@@ -48,18 +48,19 @@ def update():
     df = pick_clue(data)
     category, clue = display_clue(df)
     
-    # st.session_state.answer_button = False
     st.session_state.category = category
     st.session_state.clue = clue
     st.session_state.answer = df.target.iloc[0]
     
     # return df
     
-def find_choices():
-    all_data = pd.read_csv('clues.csv')
-    cats = all_data.groupby(by = 'category').count().reset_index()
-    options = set(cats[cats['round_'] >= 100].category)
-    st.session_state.choices = options
+# def find_choices():
+#     all_data = pd.read_csv('clues.csv')
+#     cats = all_data.groupby(by = 'category').count().reset_index()
+#     options = set(cats[cats['round_'] >= 100].category)
+#     st.session_state.choices = options
+    
+#     return st.session_state.choices
     
 if 'category' not in st.session_state:
     # 'category not in session state'
@@ -77,9 +78,14 @@ button = st.button('Show answer') #, on_click= show_answer())
 new_clue = st.button('New clue') #, on_click = update())
 
 if 'choices' not in st.session_state:
-    find_choices()
+    all_data = pd.read_csv('all_clues.csv')
+    cats = all_data.groupby(by = 'category').count().reset_index()
+    options = list(set(cats[cats['round_'] >= 100].category))
+    st.session_state['choices'] = options
 
-filter_cat = st.multiselect('Categories', st.session_state.choices, key = 'filter_cat')
+print(st.session_state)
+
+filter_cat = st.multiselect('Categories', ['geography', 'presidents'], key = 'filter_cat')
 
 if new_clue:
     update()
