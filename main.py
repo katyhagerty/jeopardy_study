99db8fed0b1f79798b1cb9d2861f3b9aee6be813
katyhagerty <katyhagerty93@gmail.com>
2023-08-26 18:17:09 -0500

    Added filter feature for rounds

--- main.py
+++ main.py
@@ -8,11 +8,7 @@ Created on Tue Aug 15 21:37:39 2023
 
 import streamlit as st
 import pandas as pd
-# from streamlit_toggle import st_toggle_switch
 
-# loc = 'all_data_v2.csv'
-
-# import streamlit as st
 from google.oauth2 import service_account
 from google.cloud import bigquery
 
@@ -21,6 +17,7 @@ from google.cloud import bigquery
 
 @st.cache_resource
 def create_connection():
+    create_connection
     credentials = service_account.Credentials.from_service_account_info(
         st.secrets["gcp_service_account"])
     client = bigquery.Client(credentials=credentials)
@@ -28,11 +25,6 @@ def create_connection():
 
 
 client = create_connection()
-
-# Perform query.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-# @st.cache_data(ttl=600)
-
 
 def run_query(query):
     # st.write(query)
@@ -42,80 +34,31 @@ def run_query(query):
     rows = [dict(row) for row in rows_raw]
     return rows
 
-
-# st.cache_data()
-# def load_data(loc):
-#     data = pd.read_csv(loc)
-
-#     if 'filter_cat' in st.session_state:
-#         if len(st.session_state.filter_cat) > 0:
-#             data = data[data.category.isin(st.session_state.filter_cat)]
-
-#     return data
-
-# data = load_data(loc)
-
-def display_clue(df):
-    category = df.category.iloc[0].upper()
-    clue = df.text.iloc[0]
-    return category, clue
-
-# def display_answer(df):
-#     return df.target.iloc[0]
-
-
-def pick_clue(data):
-    return data.sample(1)
-
-
-def show_answer():
-    # answer = df.target.iloc[0]
-    # st.session_state.answer = answer
-    # target.write(st.session_state.answer)
-    'show_answer'
-    st.session_state.answer
-    # function displays answer but nothing ever removes it afterwards
-
+# Reform list to syntax for query
+def reformat(text):
+    text = text.replace('[','(')
+    text = text.replace(']',')')
+    return text
 
 def update():
-    # data = load_data(loc)
-    # df = pick_clue(data)
-    # category, clue = display_clue(df)
+    filters = []
+    
     if 'filter_cat' in st.session_state and len(st.session_state.filter_cat) > 0:
-        # st.write('In update() if statement')
-        cats = str(st.session_state.filter_cat)
-        cats = cats.replace('[','(')
-        cats = cats.replace(']',')')
+        cats = reformat(str(st.session_state.filter_cat))
         # st.write(f'input parameter {cats}')
-        rows = run_query(
-            f"SELECT * FROM `jeopardy-396902.jeopardy.clues` WHERE category IN {cats} order by RAND() LIMIT 1")
-    else:
-        rows = run_query(
-            'SELECT * FROM `jeopardy-396902.jeopardy.clues` order by RAND() LIMIT 1')
+        filters.append(f'WHERE category IN {cats}')
+        
+    if 'filter_round' in st.session_state and len(st.session_state.filter_round) > 0:
+        rounds = reformat(str(st.session_state.filter_round))
+        filters.append(f'WHERE _round IN {rounds}')
+        
+    filters = ' AND '.join(filters)
+    
+    query_text = f'SELECT * FROM `jeopardy-396902.jeopardy.clues` {filters} order by RAND() LIMIT 1'
 
-    # if 'filter_cat' in st.session_state and len(st.session_state.filter_cat) > 0:
-    #     cats = tuple(st.session_state.filter_cat)
-    #     st.session_state.filter_cat
-    #     rows = run_query(f'SELECT * FROM `jeopardy-396902.jeopardy.clues` WHERE category IN {cats} order by RAND() LIMIT 1')
-    # else:
-    #     rows = run_query('SELECT * FROM `jeopardy-396902.jeopardy.clues` order by RAND() LIMIT 1')
-    # rows = run_query(
-        # 'SELECT * FROM `jeopardy-396902.jeopardy.clues` order by RAND() LIMIT 1')
-    # st.session_state.id =
     st.session_state.category = rows[0]['category']
     st.session_state.clue = rows[0]['text']
     st.session_state.answer = rows[0]['target']
-
-    # return df
-
-# def find_choices():
-#     all_data = pd.read_csv('clues.csv')
-#     cats = all_data.groupby(by = 'category').count().reset_index()
-#     options = set(cats[cats['round_'] >= 100].category)
-#     st.session_state.choices = options
-
-#     return st.session_state.choices
-
 
 def record():
     data = load_data(loc)
@@ -124,14 +67,7 @@ def record():
 
 
 if 'category' not in st.session_state:
-    # 'category not in session state'
-    # data = load_data(loc)
     update()
-    # clue = run_query('SELECT * FROM `jeopardy-396902.jeopardy.clues` LIMIT 1')
-    # st.session_state.id = df.index[0]
-    # st.session_state.category = category
-    # st.session_state.clue = clue
-    # st.session_state.answer = df.target.iloc[0]
 
 st.checkbox(
     label="Save",
@@ -139,33 +75,24 @@ st.checkbox(
     # label_after="Saving session",
 )
 
-# on_click called with every update even if button was not clicked prior
 button = st.button('Show answer')  # , on_click= show_answer())
 new_clue = st.button('New clue')  # , on_click = update())
-# correct = st.button('Correct', on_click = record)
 
 if 'choices' or 'rounds' not in st.session_state:
-    # all_data = pd.read_csv(loc)
-    # cats = all_data.groupby(by = 'category').count().reset_index()
-    options = pd.DataFrame(run_query(
-        'SELECT category, COUNT(category) AS count FROM `jeopardy-396902.jeopardy.clues`  group by category HAVING count > 100'))
+    options = pd.DataFrame(run_query('SELECT category, COUNT(category) AS count FROM `jeopardy-396902.jeopardy.clues`  group by category HAVING count > 100'))
+    options.sort_values(by = 'category', inplace = True)
     st.session_state['choices'] = list(options.category)
-    rounds = pd.DataFrame(
-        run_query('SELECT DISTINCT round_ FROM `jeopardy-396902.jeopardy.clues`'))
+    rounds = pd.DataFrame(run_query('SELECT DISTINCT round_ FROM `jeopardy-396902.jeopardy.clues`'))
     st.session_state['rounds'] = list(rounds.round_)
-# print(st.session_state)
 
-filter_cat = st.multiselect(
-    'Categories', st.session_state.choices, key='filter_cat')
-filter_round = st.multiselect(
-    'Round', st.session_state.rounds, key='filter_round')
+filter_cat = st.multiselect('Categories', st.session_state.choices, key='filter_cat')
+filter_round = st.multiselect('Round', st.session_state.rounds, key='filter_round')
 
 if new_clue:
     update()
 
 header = st.header(st.session_state.category)
 clue_text = st.write(st.session_state.clue)
-# target = st.empty()
 
 
 if button:
@@ -174,4 +101,4 @@ else:
     target = st.empty()
 
 
-st.session_state
+# st.session_state
