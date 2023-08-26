2685f4de0cbbe6c380bf3953400de2104683f7c2
katyhagerty <katyhagerty93@gmail.com>
2023-08-26 16:58:48 -0500

    Created function to cache database connection. run_query wasn't return unique clues because the function was cached and the input was the same. Streamlit did not realize the randomness of the input

--- main.py
+++ main.py
@@ -17,14 +17,23 @@ from google.oauth2 import service_account
 from google.cloud import bigquery
 
 # Create API client.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"]
-)
-client = bigquery.Client(credentials=credentials)
+
+
+@st.cache_resource
+def create_connection():
+    credentials = service_account.Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"])
+    client = bigquery.Client(credentials=credentials)
+    return client
+
+
+client = create_connection()
 
 # Perform query.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
+# @st.cache_data(ttl=600)
+
+
 def run_query(query):
     query_job = client.query(query)
     rows_raw = query_job.result()
@@ -36,11 +45,11 @@ def run_query(query):
 # st.cache_data()
 # def load_data(loc):
 #     data = pd.read_csv(loc)
-    
+
 #     if 'filter_cat' in st.session_state:
 #         if len(st.session_state.filter_cat) > 0:
 #             data = data[data.category.isin(st.session_state.filter_cat)]
-    
+
 #     return data
 
 # data = load_data(loc)
@@ -48,13 +57,15 @@ def run_query(query):
 def display_clue(df):
     category = df.category.iloc[0].upper()
     clue = df.text.iloc[0]
-    return category,clue
-    
+    return category, clue
+
 # def display_answer(df):
 #     return df.target.iloc[0]
-    
+
+
 def pick_clue(data):
     return data.sample(1)
+
 
 def show_answer():
     # answer = df.target.iloc[0]
@@ -63,39 +74,51 @@ def show_answer():
     'show_answer'
     st.session_state.answer
     # function displays answer but nothing ever removes it afterwards
-    
+
+
 def update():
     # data = load_data(loc)
     # df = pick_clue(data)
     # category, clue = display_clue(df)
-    
+    if 'filter_cat' in st.session_state:
+        if len(st.session_state.filter_cat) > 0:
+            cats = tuple(st.session_state.filter_cat)
+            rows = run_query(
+                f"SELECT * FROM `jeopardy-396902.jeopardy.clues` WHERE category IN {cats} order by RAND() LIMIT 1")
+    else:
+        rows = run_query(
+            'SELECT * FROM `jeopardy-396902.jeopardy.clues` order by RAND() LIMIT 1')
+
     # if 'filter_cat' in st.session_state and len(st.session_state.filter_cat) > 0:
     #     cats = tuple(st.session_state.filter_cat)
     #     st.session_state.filter_cat
     #     rows = run_query(f'SELECT * FROM `jeopardy-396902.jeopardy.clues` WHERE category IN {cats} order by RAND() LIMIT 1')
     # else:
     #     rows = run_query('SELECT * FROM `jeopardy-396902.jeopardy.clues` order by RAND() LIMIT 1')
-    rows = run_query('SELECT * FROM `jeopardy-396902.jeopardy.clues` order by RAND() LIMIT 1')
-    # st.session_state.id = 
+    rows = run_query(
+        'SELECT * FROM `jeopardy-396902.jeopardy.clues` order by RAND() LIMIT 1')
+    # st.session_state.id =
     st.session_state.category = rows[0]['category']
     st.session_state.clue = rows[0]['text']
     st.session_state.answer = rows[0]['target']
-    
+
     # return df
-    
+
 # def find_choices():
 #     all_data = pd.read_csv('clues.csv')
 #     cats = all_data.groupby(by = 'category').count().reset_index()
 #     options = set(cats[cats['round_'] >= 100].category)
 #     st.session_state.choices = options
-    
+
 #     return st.session_state.choices
+
 
 def record():
     data = load_data(loc)
     data.loc[int(st.session_state.id), 'correct'] = 1
     data.to_csv(loc)
-    
+
+
 if 'category' not in st.session_state:
     # 'category not in session state'
     # data = load_data(loc)
@@ -113,21 +136,25 @@ st.checkbox(
 )
 
 # on_click called with every update even if button was not clicked prior
-button = st.button('Show answer') #, on_click= show_answer())
-new_clue = st.button('New clue') #, on_click = update())
+button = st.button('Show answer')  # , on_click= show_answer())
+new_clue = st.button('New clue')  # , on_click = update())
 # correct = st.button('Correct', on_click = record)
 
 if 'choices' or 'rounds' not in st.session_state:
     # all_data = pd.read_csv(loc)
     # cats = all_data.groupby(by = 'category').count().reset_index()
-    options = pd.DataFrame(run_query('SELECT category, COUNT(category) AS count FROM `jeopardy-396902.jeopardy.clues`  group by category HAVING count > 100'))
+    options = pd.DataFrame(run_query(
+        'SELECT category, COUNT(category) AS count FROM `jeopardy-396902.jeopardy.clues`  group by category HAVING count > 100'))
     st.session_state['choices'] = list(options.category)
-    rounds = pd.DataFrame(run_query('SELECT DISTINCT round_ FROM `jeopardy-396902.jeopardy.clues`'))
+    rounds = pd.DataFrame(
+        run_query('SELECT DISTINCT round_ FROM `jeopardy-396902.jeopardy.clues`'))
     st.session_state['rounds'] = list(rounds.round_)
 # print(st.session_state)
 
-filter_cat = st.multiselect('Categories', st.session_state.choices, key = 'filter_cat')
-filter_round = st.multiselect('Round', st.session_state.rounds, key = 'filter_round')
+filter_cat = st.multiselect(
+    'Categories', st.session_state.choices, key='filter_cat')
+filter_round = st.multiselect(
+    'Round', st.session_state.rounds, key='filter_round')
 
 if new_clue:
     update()
@@ -135,12 +162,12 @@ if new_clue:
 header = st.header(st.session_state.category)
 clue_text = st.write(st.session_state.clue)
 # target = st.empty()
-    
+
 
 if button:
     target = st.write(st.session_state.answer)
 else:
     target = st.empty()
 
-    
+
 st.session_state
